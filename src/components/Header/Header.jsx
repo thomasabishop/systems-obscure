b7b8e125dfe9af60b6c5ebf63d38d305162d441b
thomasabishop <tactonbishop@gmail.com>
2023-01-22 19:34:27 +0000

    style: add logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import logoDark from "./sys-obs-logo-dark.svg";
+import logoLight from "./sys-obs-logo-light.svg";
 export default function Header() {
   return (
     <div className="header">
-      <Link to="/" className="header__site-title">
-        Systems Obscure
-      </Link>
+      <div className="header__wrapper">
+        <img src={logoDark} alt="logo" className="header__logo" />
+        <Link to="/" className="header__site-title">
+          Systems Obscure
+        </Link>
+      </div>
     </div>
   );
 }
