c57761e2f1ea1528ff567c6adcd3d3f064acd128
thomasabishop <tactonbishop@gmail.com>
2023-08-09 07:33:57 +0100

    style: change hours chart back to blue

--- src/components/CodingDurationsChart/CodingDurationsChart.jsx
+++ src/components/CodingDurationsChart/CodingDurationsChart.jsx
@@ -67,8 +67,8 @@ const CodingDurationsChart = ({ data, loading, error }) => {
       {
         fill: true,
         data: values,
-        borderColor: "rgba(255, 99, 132, 1)",
-        backgroundColor: "rgba(255, 99, 132, 0.3)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        backgroundColor: "rgba(54, 162, 235, 0.3)",
         borderWidth: 1,
       },
     ],
