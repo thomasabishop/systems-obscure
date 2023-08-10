5a83215a4034ebeff70fead3379cbe74af1c6c0a
thomasabishop <tactonbishop@gmail.com>
2023-08-10 14:46:14 +0100

    feature: create sessionStorage custom hook

---
+++ src/hooks/useSessionStorage.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+function useSessionStorage(key, initialValue) {
+  // Get from session storage by key
+  const storedValue = sessionStorage.getItem(key)
+  const initial = storedValue ? JSON.parse(storedValue) : initialValue
+
+  // State to store our value
+  const [value, setValue] = useState(initial)
+
+  // Return a wrapped version of useState's setter function that persists the new value to sessionStorage.
+  const setStoredValue = (newValue) => {
+    setValue(newValue)
+    sessionStorage.setItem(key, JSON.stringify(newValue))
+  }
+
+  // Remove a value from the session storage
+  const removeStoredValue = () => {
+    setValue(undefined)
+    sessionStorage.removeItem(key)
+  }
+
+  // Clear entire session storage
+  const clearSessionStorage = () => {
+    sessionStorage.clear()
+  }
+
+  return [value, setStoredValue, removeStoredValue, clearSessionStorage]
+}
+
+export default useSessionStorage
