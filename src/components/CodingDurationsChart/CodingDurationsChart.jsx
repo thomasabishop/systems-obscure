89e01c8e45225ac0fba9d2445e91b759787e044b
thomasabishop <tactonbishop@gmail.com>
2023-09-26 19:13:12 +0100

    superificial change

--- src/components/CodingDurationsChart/CodingDurationsChart.jsx
+++ src/components/CodingDurationsChart/CodingDurationsChart.jsx
@@ -59,7 +59,6 @@ const CodingDurationsChart = ({ data, loading, error }) => {
   //         "rgba(255, 206, 86, 1)",
   //         "rgba(75, 192, 192, 1)",
   //         "rgba(153, 102, 255, 1)",
-  //         "rgba(255, 159, 64, 1)",
 
   const chartJsData = {
     labels,
