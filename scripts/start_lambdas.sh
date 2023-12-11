99d2fa46dbf2424a009d24058d54b23639805c41
thomasabishop <tactonbishop@gmail.com>
2023-12-11 17:03:45 +0000

    chore: update lambda start script

--- scripts/start_lambdas.sh
+++ scripts/start_lambdas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/pocket-api; make start; exec bash'" &
-terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/functions/code-metrics; make start; exec bash'" &
-terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/functions/activities; make start; exec bash'" &
+terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/pocket-articles; make start; exec bash'" &
+terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/code-metrics; make start; exec bash'" &
+terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/activities; make start; exec bash'" &
