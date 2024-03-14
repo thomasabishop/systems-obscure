3c0aaa817c086424dbd74be32f903868d8241ae5
thomasabishop <tactonbishop@gmail.com>
2024-03-14 18:05:27 +0000

    style: readd flag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
           <h2 className="site-title">Systems Obscure</h2>
         </Link>
       </div>
-      {/* <img className="flag" src={palestine} alt="Palestine flag" /> */}
+      <img className="flag" src={palestine} alt="Palestine flag" />
     </div>
   )
 }
