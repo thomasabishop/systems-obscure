20bb86294949da46248323e7ee1c68fbc4a729f8
thomasabishop <tactonbishop@gmail.com>
2024-03-30 16:10:45 +0000

    chore: revive old lambda launch script

--- scripts/start_lambdas.sh
+++ scripts/start_lambdas.sh
@@ -2,4 +2,3 @@
 
 terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/code-stats; make start; exec bash'" &
 terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/code-durations; make start; exec bash'" &
-terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/activities; make start; exec bash'" &
