0c3a0dd76f08adf871a6750f07a20e4ed9adfa62
thomasabishop <tactonbishop@gmail.com>
2024-06-19 20:53:02 +0100

    chore: remove separate start scripts

--- scripts/start_lambdas.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/code-stats; make start; exec bash'" &
-terminator -e "bash -c 'source ~/.zshrc; cd ${HOME}/repos/lambdas/node-js/code-durations; make start; exec bash'" &
