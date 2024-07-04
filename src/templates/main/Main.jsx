41c95847c78055239e64324b96e8f51192c1c804
thomasabishop <tactonbishop@gmail.com>
2024-07-04 07:29:04 +0100

    chore: tidy

--- src/templates/main/Main.jsx
+++ src/templates/main/Main.jsx
@@ -3,10 +3,8 @@ import Header from "../../components/Header/Header"
 export default function Main(props) {
   return (
     <>
-      {/* <Container> */}
       <Header />
       <div className="main">{props.children}</div>
-      {/* </Container> */}
     </>
   )
 }
