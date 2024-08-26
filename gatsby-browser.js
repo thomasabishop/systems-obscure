3b69be5fae09102385bd02776a8e1da66881f8fd
thomasabishop <tactonbishop@gmail.com>
2024-08-26 13:59:35 +0100

    fix: attempted solution in JS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,19 @@
 require("./src/prismjs.css")
 require(`katex/dist/katex.min.css`)
+
+exports.onClientEntry = () => {
+  if (typeof window !== "undefined") {
+    // Ensure we're in the browser environment
+    function isWindowsChromeOrEdge() {
+      const userAgent = window.navigator.userAgent.toLowerCase()
+      const isChromium = /chrome|crios/.test(userAgent)
+      const isEdge = /edg/.test(userAgent)
+      const isWindows = /win/.test(userAgent)
+      return (isChromium || isEdge) && isWindows
+    }
+
+    if (isWindowsChromeOrEdge()) {
+      document.documentElement.classList.add("windows-chrome-edge")
+    }
+  }
+}
