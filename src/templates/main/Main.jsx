19c7627f90f8bd124db14900abb368c393885842
thomasabishop <tactonbishop@gmail.com>
2024-10-23 15:44:32 +0100

    feature: add not-by-AI badge

--- src/templates/main/Main.jsx
+++ src/templates/main/Main.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 import Header from "../../components/Header/Header"
+import notByAiBadge from "../../img/not-by-ai-gruvbox.svg"
+
 export default function Main(props) {
   return (
     <>
       <Header />
-      <div className="main">{props.children}</div>
+      <div className="main">
+        {props.children}
+        <img
+          style={{ width: "100px" }}
+          src={notByAiBadge}
+          alt="Written by a human, not by AI"
+        />
+      </div>
     </>
   )
 }
