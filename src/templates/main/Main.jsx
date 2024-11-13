b748dc2ff30530862f5e945844163c8d54ef1873
thomasabishop <tactonbishop@gmail.com>
2024-11-13 15:45:54 +0000

    fix: remove double ai footer thing

--- src/templates/main/Main.jsx
+++ src/templates/main/Main.jsx
@@ -3,22 +3,15 @@ import Header from "../../components/Header/Header"
 import notByAiBadge from "../../img/not-by-ai-alternative--gruvbox-material6.svg"
 
 export default function Main(props) {
-  return (
-    <>
-      <Header />
-      <div className="main">
-        {props.children}
-        <img
-          style={{ width: "100px" }}
-          src={notByAiBadge}
-          alt="Written by a human, not by AI"
-        />
-      </div>
-      <div className="footer">
-        <a href="https://hachyderm.io/@systemsobscure" target="_blank">
-          Mastodon
-        </a>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<div className="main">{props.children}</div>
+			<div className="footer">
+				<a href="https://hachyderm.io/@systemsobscure" target="_blank">
+					Mastodon
+				</a>
+			</div>
+		</>
+	)
 }
