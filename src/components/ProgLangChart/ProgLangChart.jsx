4eefbec50ba8264a51be43bb55ea043e32a624c5
thomasabishop <tactonbishop@gmail.com>
2024-11-16 10:52:19 +0000

    chore: prog lang chart start from week

--- src/components/ProgLangChart/ProgLangChart.jsx
+++ src/components/ProgLangChart/ProgLangChart.jsx
@@ -14,91 +14,91 @@ const getLanguage = (datum) => datum.language
 const getPercent = (datum) => datum.percent
 
 const ProgLangChart = ({ endpoint, reload }) => {
-  const [data, setData] = useState(null)
-  const [sessionStorage, setSessionStorage] = useSessionStorage(
-    "code_metrics_programming_languages",
-    {}
-  )
-  const [timeRange, setTimeRange] = useState("last_30_days")
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+	const [data, setData] = useState(null)
+	const [sessionStorage, setSessionStorage] = useSessionStorage(
+		"code_metrics_programming_languages",
+		{}
+	)
+	const [timeRange, setTimeRange] = useState("last_7_days")
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
 
-  const fetchData = async (timeRange) => {
-    try {
-      const response = await axios.get(`${endpoint}?timePeriod=${timeRange}`)
-      const languages = response?.data?.data?.languages
+	const fetchData = async (timeRange) => {
+		try {
+			const response = await axios.get(`${endpoint}?timePeriod=${timeRange}`)
+			const languages = response?.data?.data?.languages
 
-      let parsed = languages
-        .filter((lang) => lang.percent > 0.5)
-        .map((lang) => ({
-          language: lang.name,
-          percent: lang.percent,
-        }))
+			let parsed = languages
+				.filter((lang) => lang.percent > 0.5)
+				.map((lang) => ({
+					language: lang.name,
+					percent: lang.percent,
+				}))
 
-      setSessionStorage({
-        ...sessionStorage,
-        [timeRange]: parsed,
-      })
+			setSessionStorage({
+				...sessionStorage,
+				[timeRange]: parsed,
+			})
 
-      setData(parsed)
-      setLoading(false)
-    } catch (err) {
-      setLoading(false)
-      setError("Coding highlights could not be fetched: " + err.message + ".")
-    }
-  }
+			setData(parsed)
+			setLoading(false)
+		} catch (err) {
+			setLoading(false)
+			setError("Coding highlights could not be fetched: " + err.message + ".")
+		}
+	}
 
-  const handleTimeRangeChange = (time) => {
-    setTimeRange(time)
-  }
+	const handleTimeRangeChange = (time) => {
+		setTimeRange(time)
+	}
 
-  const handleRefreshData = async () => {
-    setSessionStorage({})
-  }
+	const handleRefreshData = async () => {
+		setSessionStorage({})
+	}
 
-  useEffect(() => {
-    handleRefreshData()
-  }, [reload])
+	useEffect(() => {
+		handleRefreshData()
+	}, [reload])
 
-  useEffect(() => {
-    setLoading(true)
-    const cachedData = sessionStorage[timeRange]
-    if (cachedData) {
-      setData(cachedData)
-      setLoading(false)
-    } else {
-      fetchData(timeRange)
-    }
-  }, [timeRange, sessionStorage])
+	useEffect(() => {
+		setLoading(true)
+		const cachedData = sessionStorage[timeRange]
+		if (cachedData) {
+			setData(cachedData)
+			setLoading(false)
+		} else {
+			fetchData(timeRange)
+		}
+	}, [timeRange, sessionStorage])
 
-  return (
-    <UiGroup
-      title="Programming languages"
-      minHeight={350}
-      controls={
-        <UiTimeControls
-          timeRange={timeRange}
-          onRangeChange={handleTimeRangeChange}
-        />
-      }
-    >
-      <UiBarChart
-        data={data}
-        error={error}
-        loading={loading}
-        barColour={orange}
-        timeRange={timeRange}
-        xMetric="language"
-        yMetric="percent"
-        yUnit="%"
-        getX={getLanguage}
-        getY={getPercent}
-        chartId="programming-languages-chart"
-      />
+	return (
+		<UiGroup
+			title="Programming languages"
+			minHeight={350}
+			controls={
+				<UiTimeControls
+					timeRange={timeRange}
+					onRangeChange={handleTimeRangeChange}
+				/>
+			}
+		>
+			<UiBarChart
+				data={data}
+				error={error}
+				loading={loading}
+				barColour={orange}
+				timeRange={timeRange}
+				xMetric="language"
+				yMetric="percent"
+				yUnit="%"
+				getX={getLanguage}
+				getY={getPercent}
+				chartId="programming-languages-chart"
+			/>
 
-      {}
-    </UiGroup>
-  )
+			{}
+		</UiGroup>
+	)
 }
 
 export default ProgLangChart
