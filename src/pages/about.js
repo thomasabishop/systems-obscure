51b9ca18f06706350afe846b7464334add828a92
thomasabishop <tactonbishop@gmail.com>
2025-01-19 19:19:29 +0000

    feat: align styles with eolas

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,81 +6,71 @@ import UiButton from "../components/UiButton/UiButton"
 
 import notByAiBadge from "../img/not-by-ai-alternative--gruvbox-material6.svg"
 export default function About() {
-	return (
-		<Main>
-			<UiGroup title="About">
-				<div className="float">
-					<img alt="A portrait of the blog author" src={portrait} width="250" />
-					<div className="caption">
-						Pictured with the WITCH computer at the{" "}
-						<a href="https://www.tnmoc.org/" target="_blank">
-							National Museum of Computing
-						</a>
-						, Bletchley Park.
-					</div>
-				</div>
-				<p>I am a self-taught software engineer from London.</p>
-				<p>
-					This blog is a technical scrapbook. I document the details of my
-					technical life so I can have a record of progress when I look back.
-					Doing this publicly motivates me to take care with my writing and to
-					be as clear as possible.{" "}
-				</p>
-				<p>
-					Currently I work for{" "}
-					<a
-						href="https://en.wikipedia.org/wiki/ITV_(TV_network)"
-						target="_blank"
-					>
-						ITV
-					</a>{" "}
-					{""}
-					as a backend software engineer. Before that I worked as a full-stack
-					software engineer at the{" "}
-					<a href="https://en.wikipedia.org/wiki/BBC">BBC</a> and as a frontend
-					engineer at <a href="https://www.arria.com/">Arria NLG</a> and in
-					several web developer roles. I did my degree in Philosophy at the
-					University of Warwick and my Postgraduate Certificate of Education at
-					Middlesex University.
-				</p>
-
-				<h3>Website theme</h3>
-				<p>
-					This is my own design and a labour of love, inspired by the interfaces
-					of the past.
-				</p>
-				<p>
-					I've drawn on many great resources in creating the UI:{" "}
-					<a href="https://jdan.github.io/98.css">98.css</a>, the{" "}
-					<a href="https://github.com/grassmunk/Chicago95">
-						Chicago95 GTK theme
-					</a>
-					, the{" "}
-					<a href="http://toastytech.com/guis/index.html">
-						Graphical User Interface Gallery
-					</a>{" "}
-					and the{" "}
-					<a href="https://www.webdesignmuseum.org/">Web Design Museum</a>, to
-					name a few.
-				</p>
-				<p>
-					The colour scheme is derived from{" "}
-					<a href="https://github.com/sainnhe/gruvbox-material">
-						Gruvbox Material
-					</a>
-					.
-				</p>
-
-				<div style={{ marginBottom: "2rem" }}></div>
-
-				{/* <img
+  return (
+    <Main>
+      <UiGroup title="About">
+        <div className="float">
+          <img alt="A portrait of the blog author" src={portrait} width="250" />
+          <div className="caption">
+            Pictured with the WITCH computer at the{" "}
+            <a href="https://www.tnmoc.org/" target="_blank">
+              National Museum of Computing
+            </a>
+            , Bletchley Park.
+          </div>
+        </div>
+        <p>I am a self-taught software engineer from London.</p>
+        <p>
+          This blog is a technical scrapbook. I document the details of my
+          technical life so I can have a record of progress when I look back.
+          Doing this publicly motivates me to take care with my writing and to
+          be as clear as possible.{" "}
+        </p>
+        <p>
+          Currently I work for{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/ITV_(TV_network)"
+            target="_blank"
+          >
+            ITV
+          </a>{" "}
+          {""}
+          as a backend software engineer. Before that I worked as a full-stack
+          engineer at the <a href="https://en.wikipedia.org/wiki/BBC">
+            BBC
+          </a>{" "}
+          and as a frontend engineer at{" "}
+          <a href="https://www.arria.com/">Arria NLG</a> and in several web
+          developer roles. I did my degree in Philosophy at the University of
+          Warwick and my Postgraduate Certificate of Education at Middlesex
+          University.
+        </p>
+        Here's my{" "}
+        <a href="https://github.com/thomasabishop" target="_blank">
+          GitHub
+        </a>{" "}
+        and{" "}
+        <a href="https://hachyderm.io/@systemsobscure" target="_blank" rel="me">
+          Mastodon
+        </a>{" "}
+        accounts.
+        <h3>Website theme</h3>
+        <p>
+          This is my own design and however the colour scheme is derived from{" "}
+          <a href="https://github.com/sainnhe/gruvbox-material">
+            Gruvbox Material
+          </a>
+          .
+        </p>
+        <div style={{ marginBottom: "2rem" }}></div>
+        {/* <img
           style={{ width: "100px" }}
           src={notByAiBadge}
           alt="Written by a human, not by AI"
         /> */}
-			</UiGroup>
-		</Main>
-	)
+      </UiGroup>
+    </Main>
+  )
 }
 
 export const Head = () => <title>About | Systems Obscure</title>
--- src/templates/main/Main.jsx
+++ src/templates/main/Main.jsx
@@ -3,21 +3,18 @@ import Header from "../../components/Header/Header"
 import notByAiBadge from "../../img/not-by-ai-alternative--gruvbox-material6.svg"
 
 export default function Main(props) {
-	return (
-		<div style={{ paddingBottom: "1rem" }}>
-			<Header />
-			<div className="main">{props.children}</div>
-			<div className="footer">
-				{/*
-				<a href="https://github.com/thomasabishop" target="_blank">
+  return (
+    <div style={{ paddingBottom: "1rem" }}>
+      <Header />
+      <div className="main">{props.children}</div>
+      {/* <div className="footer">
+        <a href="https://github.com/thomasabishop" target="_blank">
           GitHub
         </a>
-				*/}
-
-				<a rel="me" href="https://hachyderm.io/@systemsobscure" target="_blank">
-					Mastodon
-				</a>
-			</div>
-		</div>
-	)
+        <a rel="me" href="https://hachyderm.io/@systemsobscure" target="_blank">
+          Mastodon
+        </a>
+      </div> */}
+    </div>
+  )
 }
